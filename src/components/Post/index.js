a747ff788364c3e7b55f1e94629b4ee3a7a8ba4d
kashif ansari <mhd.kashif1@gmail.com>
2023-04-17 03:20:47 +0530

    added bottom container and right container

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,7 +1,11 @@
 import React, {useState} from 'react';
-import {View, Text, TouchableWithoutFeedback} from 'react-native';
+import {View, Text, TouchableWithoutFeedback, Image} from 'react-native';
 import Video from 'react-native-video';
 import styles from './styles';
+import Entypo from 'react-native-vector-icons/Entypo';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import FontAwesom from 'react-native-vector-icons/FontAwesome';
+import Fontisto from 'react-native-vector-icons/Fontisto';
 
 const Post = () => {
   const [pauseed, setPaused] = useState(false);
@@ -12,15 +16,56 @@ const Post = () => {
   return (
     <View style={styles.container}>
       <TouchableWithoutFeedback onPress={onPlayPausePress}>
-        <Video
-          source={{
-            uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4',
-          }}
-          style={styles.video}
-          resizeMode={'cover'}
-          repeat={true}
-          paused={pauseed}
-        />
+        <View>
+          <Video
+            source={{
+              uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4',
+            }}
+            style={styles.video}
+            resizeMode={'cover'}
+            repeat={true}
+            paused={pauseed}
+          />
+          <View style={styles.UIContainer}>
+            <View style={styles.rightContainer}>
+              <Image
+                style={styles.profilePicture}
+                source={{
+                  uri: 'https://media.licdn.com/dms/image/D4D03AQFYUUBfF9Krkg/profile-displayphoto-shrink_800_800/0/1678732156344?e=2147483647&v=beta&t=qK5TT8wnhpgt-ftrh6o6xDAz_MT0BS6q4i5IgZKElXg',
+                }}
+              />
+              <View style={styles.iconContainer}>
+                <AntDesign name={'hearto'} size={40} color="white" />
+                <Text style={styles.statusLabel}>13</Text>
+              </View>
+              <View style={styles.iconContainer}>
+                <FontAwesom name={'commenting'} size={40} color="white" />
+                <Text style={styles.statusLabel}>13</Text>
+              </View>
+              <View style={styles.iconContainer}>
+                <Fontisto name={'share-a'} size={40} color="white" />
+                <Text style={styles.statusLabel}>13</Text>
+              </View>
+            </View>
+            <View style={styles.bottomContainer}>
+              <View>
+                <Text style={styles.userHandle}>@KashifAnsari</Text>
+                <Text style={styles.discription}>song by hahah</Text>
+                <View style={styles.musicalRow}>
+                  <Entypo name={'beamed-note'} size={24} color="white" />
+                  <Text style={styles.songName}>nf search</Text>
+                </View>
+              </View>
+
+              <Image
+                style={styles.somgImage}
+                source={{
+                  uri: 'https://media.licdn.com/dms/image/D4D03AQFYUUBfF9Krkg/profile-displayphoto-shrink_800_800/0/1678732156344?e=2147483647&v=beta&t=qK5TT8wnhpgt-ftrh6o6xDAz_MT0BS6q4i5IgZKElXg',
+                }}
+              />
+            </View>
+          </View>
+        </View>
       </TouchableWithoutFeedback>
     </View>
   );
--- src/components/Post/styles.js
+++ src/components/Post/styles.js
@@ -6,7 +6,6 @@ import {
 
 const styles = StyleSheet.create({
   video: {
-   
     position: 'absolute',
     top: 0,
     left: 0,
@@ -15,7 +14,62 @@ const styles = StyleSheet.create({
   },
   container: {
     width: '100%',
-    height: hp('100%'),
+    height: Dimensions.get('window').height,
   },
+  UIContainer: {
+    height: '100%',
+    justifyContent: 'flex-end',
+  },
+  rightContainer: {
+    alignSelf: 'flex-end',
+    height: 300,
+    justifyContent: 'space-between',
+  },
+  bottomContainer: {
+    padding: 15,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems:'flex-end',
+  },
+  profilePicture: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    borderWidth: 2,
+    borderColor: '#fff',
+  },
+  somgImage: {
+    width: 35,
+    height: 35,
+    borderRadius: 25,
+    borderWidth: 2,
+    borderColor: '#fff',
+  },
+  statusLabel: {
+    color: '#fff',
+    alignSelf: 'center',
+  },
+  userHandle: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 700,
+    marginBottom: 5,
+  },
+  discription: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 300,
+    marginBottom: 5,
+  },
+  songName: {
+    color: '#fff',
+    fontSize: 16,
+    marginLeft: 5,
+  },
+  musicalRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  profilePictureContainer: {},
 });
 export default styles;
